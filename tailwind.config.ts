00bc73443e6604c3ad62228b4f8bd7872def792f
Philip Lee <philiplee1999@gmail.com>
2024-07-25 09:08:09 -0700

    fixed merge errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import type { Config } from "tailwindcss"
 
 const config = {
@@ -79,25 +78,4 @@ const config = {
 } satisfies Config
 
 export default config
-=======
-import type { Config } from "tailwindcss";
 
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
->>>>>>> 948cb63c6831dd78b2cab6bc2ae8e1ce8b78877e
